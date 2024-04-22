242ccc73d7f0c337025261c57c4e726f8e731a8c
Hughen Flint <104950296+FlashBangAU@users.noreply.github.com>
2024-04-22 23:30:55 +1000

    Server Coordinator code update

--- src/Servers/ServerCoordinator.java
+++ src/Servers/ServerCoordinator.java
@@ -8,8 +8,8 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 
 /**
  *
@@ -18,86 +18,61 @@ import java.net.UnknownHostException;
 public class ServerCoordinator {
     //This class is to be used by TcpServer class, not public.
 
-    class Connection extends Thread {
-
-        DataInputStream in;
-        DataOutputStream out;
-        Socket clientSocket;
-
-        //connection created
-        public Connection(Socket aClientSocket) {
-            try {
-                clientSocket = aClientSocket;
-                in = new DataInputStream(
-                        clientSocket.getInputStream());
-                out = new DataOutputStream(
-                        clientSocket.getOutputStream());
-                this.start();
-            } catch (IOException e) {
-                System.out.println("Connection:" + e.getMessage());
+    public static void main(String args[]) {
+        //Server waits for client to connect on same port
+        try {
+            final int serverPort = 3001;
+            ServerSocket listenSocket = new ServerSocket(serverPort);
+            while (true) {
+                //listening for client
+                Socket clientSocket = listenSocket.accept();
+                Connection c = new Connection(clientSocket);
+                System.out.println("Recieving data from client: " + clientSocket.getPort());
             }
+        } catch (IOException e) {
+            System.out.println("Listen :" + e.getMessage());
         }
+    }
+}
 
-        //retreives data, saves data and sends reply
-        public void run() {
-            Socket s = null;
-            final String hostName = "localhost";
-            int serverPort = 0;
-            String response = "Error happened with ServerCoordinator";
+//This class is to be used by TcpServer class, not public.
+class Connection extends Thread {
 
-            try { // an echo server
-                //data recieved
-                String data = in.readUTF();
+    DataInputStream in;
+    DataOutputStream out;
+    Socket clientSocket;
 
-                String[] orderArray = data.split(":");
-                if (orderArray[0].equals("1")) {
-                    serverPort = 3001;
-                } else if (orderArray[0].equals("2")) {
-                    serverPort = 3002;
-                }
+    //connection created
+    public Connection(Socket aClientSocket) {
+        try {
+            clientSocket = aClientSocket;
+            in = new DataInputStream(
+                    clientSocket.getInputStream());
+            out = new DataOutputStream(
+                    clientSocket.getOutputStream());
+            this.start();
+        } catch (IOException e) {
+            System.out.println("Connection:" + e.getMessage());
+        }
+    }
 
-                try {
-                    //connection is made
-                    s = new Socket(hostName, serverPort);
-                    DataInputStream in = new DataInputStream(s.getInputStream());
-                    DataOutputStream out = new DataOutputStream(s.getOutputStream());
+    //retreives data, saves data and sends reply
+    public void run() {
+        try { // an echo server
+            //data recieved
+            String data = in.readUTF();
 
-                    //send message
-                    out.writeUTF(data);
-                    System.out.println("Sending Data to Movie or Book Server..............");
+            //data is sent back to client
+            out.writeUTF("Save Data of the member number: ");
 
-                    //server response
-                    response = in.readUTF();
-                    System.out.println("Server Response: ");
-
-                    //counter for how many clients have been entered while client has been running
-                } catch (UnknownHostException e) {
-                    System.out.println("Sock:" + e.getMessage());
-                } catch (EOFException e) {
-                    System.out.println("EOF:" + e.getMessage());
-                } catch (IOException e) {
-                    System.out.println("IO:" + e.getMessage());
-                } finally {
-                    if (s != null)
-	     try {
-                        s.close();
-                    } catch (IOException e) {
-                        System.out.println("close:" + e.getMessage());
-                    }
-                }
-
-                //data is sent back to client
-                out.writeUTF(response);
-
-            } catch (EOFException e) {
-                System.out.println("EOF:" + e.getMessage());
-            } catch (IOException e) {
-                System.out.println("IO:" + e.getMessage());
-            } finally {
-                try {
-                    clientSocket.close();
-                } catch (IOException e) {/*close failed*/
-                }
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        } finally {
+            try {
+                clientSocket.close();
+            } catch (IOException e) {/*close failed*/
             }
         }
     }
