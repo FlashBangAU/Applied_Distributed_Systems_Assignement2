5a9ad1b57d0e3af64bac82e101b460ea9c42834e
Hughen Flint <104950296+FlashBangAU@users.noreply.github.com>
2024-04-30 00:06:22 +1000

    Task.java implemented and functions OrderClient added Task function and small changes to send object ServerCoordinator now has client side for Book and Movie Server ServerBook implemented and has excuteTask implemented BookOrder and MovieOrder have getResult and excuteTask implemented 	slight adjustment required with return string

--- src/Servers/ServerCoordinator.java
+++ src/Servers/ServerCoordinator.java
@@ -8,8 +8,16 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import orderItem.BookOrder;
+import orderItem.MovieOrder;
+import orderItem.Task;
 
 /**
  *
@@ -21,7 +29,7 @@ public class ServerCoordinator {
     public static void main(String args[]) {
         //Server waits for client to connect on same port
         try {
-            final int serverPort = 3001;
+            final int serverPort = 3000;
             ServerSocket listenSocket = new ServerSocket(serverPort);
             while (true) {
                 //listening for client
@@ -38,18 +46,16 @@ public class ServerCoordinator {
 //This class is to be used by TcpServer class, not public.
 class Connection extends Thread {
 
-    DataInputStream in;
-    DataOutputStream out;
+    ObjectInputStream in;
+    ObjectOutputStream out;
     Socket clientSocket;
 
     //connection created
     public Connection(Socket aClientSocket) {
         try {
             clientSocket = aClientSocket;
-            in = new DataInputStream(
-                    clientSocket.getInputStream());
-            out = new DataOutputStream(
-                    clientSocket.getOutputStream());
+            in = new ObjectInputStream(clientSocket.getInputStream());
+            out = new ObjectOutputStream(clientSocket.getOutputStream());
             this.start();
         } catch (IOException e) {
             System.out.println("Connection:" + e.getMessage());
@@ -60,20 +66,68 @@ class Connection extends Thread {
     public void run() {
         try { // an echo server
             //data recieved
-            String data = in.readUTF();
+            Task t = (Task) in.readObject();
+            System.out.println(t);
+            if (t.toString().contains("BookOrder")) {
+                System.out.print("Sending data to ServerBook");
+                t = (Task) navigateClient(3001);
+            } else if (t.toString().contains("MovieOrder")) {
+                System.out.print("Sending data to ServerMovie");
+            }
+            t.getResult();
 
             //data is sent back to client
-            out.writeUTF("Save Data of the member number: ");
+            out.writeObject(t);
 
         } catch (EOFException e) {
             System.out.println("EOF:" + e.getMessage());
         } catch (IOException e) {
-            System.out.println("IO:" + e.getMessage());
+            System.out.println("readline:" + e.getMessage());
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
         } finally {
             try {
                 clientSocket.close();
             } catch (IOException e) {/*close failed*/
             }
         }
+    }
+
+    public static Object navigateClient(int serverPort) throws ClassNotFoundException {
+        Socket s = null;
+        Task t = null;
+        String hostName = "localhost";
+        try {
+
+            s = new Socket(hostName, serverPort);
+
+            ObjectInputStream in = null;
+            ObjectOutputStream out = null;
+
+            out = new ObjectOutputStream(s.getOutputStream());
+            in = new ObjectInputStream(s.getInputStream());
+            
+            //send message
+            System.out.println("SENDING OBJECT TO SERVER..........");
+            out.writeObject(t);
+
+            System.out.println("RECIEVING COMPUTED OBJECT FROM SERVER........");
+            t = (Task) in.readObject();
+
+        } catch (UnknownHostException e) {
+            System.out.println("Sock:" + e.getMessage());
+        } catch (EOFException e) {
+            System.out.println("EOF:" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IO:" + e.getMessage());
+        } finally {
+            if (s != null)
+	     try {
+                s.close();
+            } catch (IOException e) {
+                System.out.println("close:" + e.getMessage());
+            }
+        }
+        return t;
     }
 }
